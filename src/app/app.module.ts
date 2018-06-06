cbb9dc332512b35e3903beb7fc2b9265aa24eff3
Oscar Salcedo <josueht15@gmail.com>
2018-06-06 16:37:11 -0500

    aspecto visual creado, falta n los graficos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { DatosComponent } from './components/datos/datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegistrarComponent,
+    DatosComponent
   ],
   imports: [
     BrowserModule
